a81224a0fbee45abe4243324f032a976f229e4b5
TinD <tindao2011@gmail.com>
2025-03-30 18:29:19 -0400

    config file

---
+++ src/platform/config.hpp
@@ -0,0 +1,14 @@
+// Hamster config
+
+#pragma once
+
+
+// The size of each page
+#define HAMSTER_PAGE_SIZE 256
+
+// The number of pages loaded in RAM at once
+#define HAMSTER_CONCUR_PAGES 16
+
+// The absolute maximum number of pages
+// RAM pages + swapped pages
+#define HAMSTER_MAX_PAGES 1024
