925b92c5cfe0b74cd99deae49c3a6f09154303cf
TinD <tindao2011@gmail.com>
2025-04-01 18:46:19 -0400

    make sure page size is power of 2

--- src/platform/config.hpp
+++ src/platform/config.hpp
@@ -5,6 +5,7 @@
 
 // The size of each page
 #define HAMSTER_PAGE_SIZE 256
+static_assert((HAMSTER_PAGE_SIZE & (HAMSTER_PAGE_SIZE - 1)) == 0, "Page size must be a power of 2");
 
 // The number of pages loaded in RAM at once
 #define HAMSTER_CONCUR_PAGES 16
