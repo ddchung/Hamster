be5df9da03bfe173b44af34443da5ca531a3d303
TinD <tindao2011@gmail.com>
2025-04-02 16:44:39 -0400

    basic thread and process

---
+++ src/process/process.cpp
@@ -0,0 +1,39 @@
+// implementation of process.hpp
+
+
+#include <process/process.hpp>
+#include <process/thread.hpp>
+#include <memory/allocator.hpp>
+
+namespace Hamster
+{
+    Thread::RunCode Process::tick_all()
+    {
+        memory_space.swap_in_pages();
+        for (auto thread : threads)
+        {
+            if (thread.running)
+            {
+                thread.thread->tick();
+            }
+        }
+        memory_space.swap_out_pages();
+    }
+
+    Thread::RunCode Process::tick_all64()
+    {
+        memory_space.swap_in_pages();
+        for (int i = 0; i < 64; i++)
+        {
+            for (auto thread : threads)
+            {
+                if (thread.running)
+                {
+                    thread.thread->tick();
+                }
+            }
+        }
+        memory_space.swap_out_pages();
+    }
+} // namespace Hamster
+
---
+++ src/process/process.hpp
@@ -0,0 +1,50 @@
+// A Process
+
+#pragma once
+
+#include <memory/memory_space.hpp>
+#include <memory/stl_sequential.hpp>
+#include <process/thread.hpp>
+
+#include <memory/allocator.hpp>
+#include <utility>
+
+namespace Hamster
+{
+    class Process
+    {
+        struct ThreadInfo
+        {
+            Thread *thread;
+
+            // not used yet
+            bool running : 1;
+        };
+    public:
+        // Make a thread
+        // ThreadClass: a class derived from Thread
+        // ThreadClass must have a constructor
+        // accepting a MemorySpace reference as the first argument
+        template <class ThreadClass, typename... Args>
+        int create_thread(Args&&...);
+
+        Thread::RunCode tick_all();
+        Thread::RunCode tick_all64();
+
+    private:
+        MemorySpace memory_space;
+
+        Vector<ThreadInfo> threads;
+    };
+} // namespace Hamster
+
+template <class ThreadClass, typename... Args>
+int Hamster::Process::create_thread(Args&&... args)
+{
+    Thread *thread = alloc<Thread>(memory_space, std::forward<Args>(args)...);
+    ThreadInfo info;
+    info.thread = thread;
+    info.running = true;
+    threads.push_back(info);
+    return 0;
+}
