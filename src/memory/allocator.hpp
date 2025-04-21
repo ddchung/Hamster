ea8aeaaab688cdb7e096eacec6ae31b7d32d2a09
TinD <tindao2011@gmail.com>
2025-04-21 10:21:10 -0400

    fix buggy allocator

--- src/memory/allocator.hpp
+++ src/memory/allocator.hpp
@@ -6,43 +6,49 @@
 #include <cassert>
 #include <cstdint>
 #include <new>
+#include <type_traits>
 
+#include <cstdio>
 
-namespace Hamster 
+namespace Hamster
 {
     template <typename T, typename... Args>
-    T* alloc(unsigned int N = 1, Args&&... args)
+    T *alloc(unsigned int N = 1, Args &&...args)
     {
-        constexpr unsigned int additional = (sizeof(unsigned int) / sizeof(T)) == 0 ? 1 : (sizeof(unsigned int) / sizeof(T));
+        constexpr unsigned int additional = (sizeof(unsigned int) + sizeof(T) - 1) / sizeof(T);
+        void *raw_ptr = _malloc(sizeof(T) * (N + additional));
+        assert(raw_ptr != nullptr);
 
-        void *ptr = _malloc(sizeof(T) * (N + additional));
-        assert(ptr != nullptr);
-        assert(reinterpret_cast<uintptr_t>(ptr) % alignof(T) == 0);
-        assert(reinterpret_cast<uintptr_t>(ptr) % alignof(unsigned int) == 0);
+        uintptr_t raw_addr = reinterpret_cast<uintptr_t>(raw_ptr);
+        assert(raw_addr % alignof(T) == 0);
+        assert(raw_addr % alignof(unsigned int) == 0);
 
-        *reinterpret_cast<unsigned int*>(ptr) = N;
-        ptr = (T*)ptr + additional;
+        auto *meta_ptr = reinterpret_cast<unsigned int *>(raw_ptr);
+        *meta_ptr = N;
 
-        for (unsigned int i = 0; i < N; ++i) {
-            new (static_cast<T*>(ptr) + i) T(std::forward<Args>(args)...);
+        T *user_ptr = reinterpret_cast<T *>(meta_ptr + additional); // safely skip metadata
+        for (unsigned int i = 0; i < N; ++i)
+        {
+            new (user_ptr + i) T(std::forward<Args>(args)...);
         }
-        return static_cast<T*>(ptr);
+        return user_ptr;
     }
 
     template <typename T>
-    void dealloc(T* ptr) {
-        if (ptr == nullptr)
+    void dealloc(T *ptr)
+    {
+        if (!ptr)
             return;
 
-        constexpr unsigned int additional = (sizeof(unsigned int) / sizeof(T)) == 0 ? 1 : (sizeof(unsigned int) / sizeof(T));
+        constexpr unsigned int additional = (sizeof(unsigned int) + sizeof(T) - 1) / sizeof(T);
+        unsigned int *meta_ptr = (unsigned int *)(ptr) - additional;
+        unsigned int N = *meta_ptr;
 
-        ptr -= additional;
-
-        unsigned int N = *(unsigned int *)(ptr);
-
-        for (unsigned int i = 0; i < N; ++i) {
-            ((T*)ptr)[i].~T();
+        for (unsigned int i = 0; i < N; ++i)
+        {
+            ptr[i].~T();
         }
-        _free((void*)ptr);
+
+        _free(meta_ptr);
     }
 } // namespace Hamster
