c9fd5b652cb191bb364b7e72f1db7ce4a4dee626
TinD <tindao2011@gmail.com>
2025-04-21 19:21:36 -0400

    make allocator sanitizer

--- src/memory/allocator.hpp
+++ src/memory/allocator.hpp
@@ -8,10 +8,28 @@
 #include <new>
 #include <type_traits>
 
+#ifndef NDEBUG
+
+#include <unordered_set>
 #include <cstdio>
+#include <cstdlib>
+
+#if __cplusplus >= 202002L
+# include <source_location>
+#endif // __cplusplus >= 202002L
+
+#endif // NDEBUG
 
 namespace Hamster
 {
+#ifndef NDEBUG
+    namespace
+    {
+        std::unordered_set<void *> allocated_pointers;
+    } // namespace
+#endif // NDEBUG
+    
+
     template <typename T, typename... Args>
     T *alloc(unsigned int N = 1, Args &&...args)
     {
@@ -27,6 +45,12 @@ namespace Hamster
         *meta_ptr = N;
 
         T *user_ptr = reinterpret_cast<T *>(meta_ptr) + additional; // safely skip metadata
+    
+#ifndef NDEBUG
+        assert(allocated_pointers.find(user_ptr) == allocated_pointers.end());
+        allocated_pointers.insert(user_ptr);
+#endif // NDEBUG
+
         for (unsigned int i = 0; i < N; ++i)
         {
             new (user_ptr + i) T(std::forward<Args>(args)...);
@@ -35,10 +59,29 @@ namespace Hamster
     }
 
     template <typename T>
-    void dealloc(T *ptr)
+    void dealloc(T *ptr
+#if !defined(NDEBUG) && __cplusplus >= 202002L
+        , std::source_location loc = std::source_location::current()
+#endif // !defined(NDEBUG) && __cplusplus >= 202002L
+        )
     {
         if (!ptr)
             return;
+
+#ifndef NDEBUG
+        if (allocated_pointers.find(ptr) == allocated_pointers.end())
+        {
+# if __cplusplus >= 202002L
+            printf("Error: double free detected at %s:%d\n", loc.file_name(), (int)loc.line());
+# else // __cplusplus >= 202002L
+            printf("Error: double free detected\n");
+# endif // __cplusplus >= 202002L
+            printf("Pointer: %p\n", (void*)ptr);
+            abort();
+            return;
+        }
+        allocated_pointers.erase(ptr);
+#endif // NDEBUG
 
         constexpr unsigned int additional = (sizeof(unsigned int) + sizeof(T) - 1) / sizeof(T);
         unsigned int *meta_ptr = (unsigned int *)(ptr - additional);
