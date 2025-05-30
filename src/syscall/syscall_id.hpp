c2dd50d5c8206712babac2df91d4d72a6420e429
TinD <tindao2011@gmail.com>
2025-05-30 18:59:12 -0400

    update system call numbers to better match linux

--- src/syscall/syscall_id.hpp
+++ src/syscall/syscall_id.hpp
@@ -20,29 +20,35 @@ namespace Hamster
     namespace SyscallID
     {
         // Newlib C system calls
+        // Note that ones with RISC-V Linux equivelants use
+        // that ID, but ones that don't have such an equivalent
+        // use an ID starting from 512
+        // 
+        // Ones without an equivelant are:
+        // FORK FSTAT ISATTY LINK LSEEK OPEN UNLINK WAIT
         enum ID : uint16_t
         {
-            EXIT = 0,
-            CLOSE = 1,
+            EXIT = 93,
+            CLOSE = 57,
             // environ not supported yet, but skip 2 for
             // future compatibility
-            EXECVE = 3,
-            FORK = 4,
-            FSTAT = 5,
-            GETPID = 6,
-            ISATTY = 7,
-            KILL = 8,
-            LINK = 9,
-            LSEEK = 10,
-            OPEN = 11,
-            READ = 12,
+            EXECVE = 221,
+            FORK = 512,
+            FSTAT = 513,
+            GETPID = 172,
+            ISATTY = 514,
+            KILL = 129,
+            LINK = 515,
+            LSEEK = 516,
+            OPEN = 517,
+            READ = 63,
             // sbrk is not needed, as processes are free
             // to write to any memory they want, for now
             // but we skip 13 for future compatibility
-            TIMES = 14,
-            UNLINK = 15,
-            WAIT = 16,
-            WRITE = 17,
+            TIMES = 153,
+            UNLINK = 518,
+            WAIT = 519,
+            WRITE = 64,
         };
     } // namespace SyscallID
     
