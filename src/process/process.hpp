7911b316830435e6b3f2ba12bacbc444c2f37324
TinD <tindao2011@gmail.com>
2025-07-06 18:01:08 -0400

    add a dfd argument to load elf

--- src/process/process.hpp
+++ src/process/process.hpp
@@ -29,11 +29,12 @@ namespace Hamster
          * @param argv Arguments to pass to the program. Note that by convention, the first argument is
          *      * the program name
          * @param envp Environment variables to pass to the program
+         * @param dirfd Path is relative to this, or if not set, to the root directory
          * @warning This will kill all threads and overwrite the memory space, and create
          *        * a single new thread with the entry point of the ELF file
          * @note If `fds` is empty, it will open `/dev/console` 3 times for stdin, stdout, and stderr
          */
-        int load_elf(const char *path, const char *const *argv = 0, const char *const *envp = 0);
+        int load_elf(const char *path, const char *const *argv = 0, const char *const *envp = 0, int dirfd = -1);
         
         MemorySpace memory_space;
         UnorderedMap<uint32_t, size_t> reserved_mem; // For `lr` and `sc` instructions
