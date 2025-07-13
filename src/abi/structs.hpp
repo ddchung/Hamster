a6719eb28a083ecfa715dc0d1d8305a9880a9ec3
TinD <tindao2011@gmail.com>
2025-07-13 12:18:10 -0400

    statx system call

--- src/abi/structs.hpp
+++ src/abi/structs.hpp
@@ -45,6 +45,17 @@ namespace Hamster
             struct
             {
                 uint32_t addr;
+
+                union {
+                    int16_t addr_lsb;
+                    struct {
+                        char _pad[4];
+                        uint32_t lower;
+                        uint32_t upper;
+                    } addr_bnd;
+
+                    // PKUERR and TRAP_PERF aren't used for now
+                };
             } fault;
 
             struct
@@ -107,6 +118,36 @@ namespace Hamster
         int32_t ctime;
         uint32_t ctime_nsec;
         uint32_t __unused4, __unused5;
+    };
+
+    struct sys_statx
+    {
+        uint32_t mask;
+        uint32_t blksize;
+        uint64_t attributes;
+        uint32_t nlink;
+        uint32_t uid;
+        uint32_t gid;
+        uint16_t mode;
+        uint16_t __spare0[1];
+        uint64_t ino;
+        uint64_t size;
+        uint64_t blocks;
+        uint64_t attr_mask;
+
+        struct {
+            int64_t sec;
+            uint32_t nsec;
+            int32_t __reserved;
+        } atime, btime, ctime, mtime;
+        uint32_t rdev_major;
+        uint32_t rdev_minor;
+        uint32_t dev_major;
+        uint32_t dev_minor;
+        uint64_t mnt_id;
+        uint32_t dio_memalign;
+        uint32_t dio_offsetalign;
+        uint64_t __spare3[12];
     };
 
     struct sys_dirent
