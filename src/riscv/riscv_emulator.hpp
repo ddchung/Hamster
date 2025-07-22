649a5d9181db561f8a9c456e868eeef66ab3ceac
TinD <tindao2011@gmail.com>
2025-07-22 19:07:07 -0400

    RISC-V Emulator

---
+++ src/riscv/riscv_emulator.hpp
@@ -0,0 +1,83 @@
+// Hamster risc-v emulator
+
+#pragma once
+
+#include <memory/memory_space.hpp>
+#include <memory/stl_map.hpp>
+#include <cstdint>
+
+namespace Hamster
+{
+    struct EmulatorMemory
+    {
+        MemorySpace memory;
+        UnorderedMap<uint32_t, int> reserved_mem;
+    };
+
+    class RiscVEmulator
+    {
+    public:
+        uint32_t x[32];
+        double f[32];
+        uint32_t fcsr;
+        uint32_t pc;
+        
+        int reserved_mem_id;
+
+        EmulatorMemory *memory;
+
+        struct ExecuteResult
+        {
+            enum class Status : uint8_t
+            {
+                Success,
+                IllegalInstruction,
+                IllegalLoad,
+                IllegalStore,
+                ECALL,
+                EBREAK,
+                Error,
+            };
+
+            Status status;
+
+            union 
+            {
+                struct
+                {
+                    uint32_t instruction;
+                } illegal_instruction;
+
+                struct
+                {
+                    uint32_t address;
+                } illegal_load;
+
+                struct
+                {
+                    uint32_t address;
+                    uint32_t value;
+                } illegal_store;
+            };
+        };
+
+        /**
+         * @brief Executes a single instruction at the current PC
+         * @return ExecuteResult indicating the outcome of the execution
+         */
+        ExecuteResult execute();
+    private:
+        
+        int read8(uint32_t addr, uint8_t &out);
+        int read16(uint32_t addr, uint16_t &out);
+        int read32(uint32_t addr, uint32_t &out);
+        int readf32(uint32_t addr, float &out);
+        int readf64(uint32_t addr, double &out);
+        int write8(uint32_t addr, uint8_t value);
+        int write16(uint32_t addr, uint16_t value);
+        int write32(uint32_t addr, uint32_t value);
+        int writef32(uint32_t addr, float value);
+        int writef64(uint32_t addr, double value);
+    };
+} // namespace Hamster
+
