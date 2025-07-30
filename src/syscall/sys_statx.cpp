8f1e49d6e5e0eac63ba67a3d846e41083e23c454
TinD <tindao2011@gmail.com>
2025-07-30 07:34:55 -0400

    fix statx

--- src/syscall/sys_statx.cpp
+++ src/syscall/sys_statx.cpp
@@ -94,7 +94,7 @@ namespace Hamster
         statxbuf.ctime.nsec = statbuf.ctime_nsec;
 
         // Copy to user memory
-        if (!task->memory->obj.memory.memcpy(statxbuf_loc, &statxbuf, sizeof(statxbuf)))
+        if (task->memory->obj.memory.memcpy(statxbuf_loc, &statxbuf, sizeof(statxbuf)) < 0)
         {
             error = EFAULT; // Bad address
             return cvt_error();
