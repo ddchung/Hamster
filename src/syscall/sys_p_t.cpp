87cbf46fa2d7819ad58b59aae27ec959e2ec495a
TinD <tindao2011@gmail.com>
2025-10-15 18:42:31 -0400

    Read and write system calls

---
+++ src/syscall/sys_p_t.cpp
@@ -0,0 +1,101 @@
+// Hamster P-T system calls
+
+#include <syscall/syscall.hpp>
+#include <process/task.hpp>
+#include <process/task_vfs_fd.hpp>
+#include <abi/values.hpp>
+#include <abi/structs.hpp>
+
+namespace Hamster
+{
+    int32_t sys_read(Task &task, int32_t fd, uint32_t buf_loc, uint32_t count)
+    {
+        BaseTaskFD *user_fd = task.get_fd_table()->get_fd(fd);
+        if (!user_fd)
+            return cvt_error();
+
+        int fd_flags = user_fd->get_flags();
+
+        // Copy as many times as needed
+
+        size_t total_read = 0;
+
+        while (count > 0)
+        {
+            // Read at most to the end of the page
+            uint32_t location = buf_loc + total_read;
+            size_t to_read = std::min(count, HAMSTER_PAGE_SIZE - ((location) % HAMSTER_PAGE_SIZE));
+            uint8_t *mem = task.get_memory().make_iterator_1(location);
+            if (!mem)
+            {
+                if (total_read > 0)
+                    return total_read;
+                return cvt_error();
+            }
+
+            ssize_t bytes_read = user_fd->read(mem, to_read);
+
+            if (bytes_read < 0)
+            {
+                if (total_read > 0)
+                {
+                    // Return the total bytes read so far
+                    return total_read;
+                }
+
+                if (error == H_EAGAIN)
+                {
+                    // Blocking read, block only if O_NONBLOCK isn't set and we aren't already
+                    // blocking
+                    if ((fd_flags & OPEN_NONBLOCK) == 0 && !task.is_blocking())
+                    {
+                        task.get_blocking_saved()[0] = fd;
+                        task.block([](Task &task) {
+                            int32_t blocking_fd = task.get_blocking_saved()[0];
+
+                            BaseTaskFD *task_fd = task.get_fd_table()->get_fd(blocking_fd);
+                            int res;
+
+                            if (!task_fd)
+                                res = -1;
+                            else
+                                res = task_fd->poll(0x1); // READ
+
+                            if (res == 0)
+                                return; // not ready
+                            else if (res == 1)
+                            {
+                                task.get_emulator().x[10];
+                                task.get_emulator().x[10] = syscall(task, sys_read);
+                                if ((int32_t)task.get_emulator().x[10] == -H_EAGAIN)
+                                    return;
+                            }
+                            else
+                            {
+                                // error
+                                task.get_emulator().x[10] = res;
+                            }
+
+
+                            task.end_block();
+                        });
+
+                        return 0;
+                    }
+
+                    return -H_EAGAIN;
+                }
+                return cvt_error(); // Return error if it isn't EAGAIN
+            }
+
+            if (bytes_read == 0)
+                break;
+
+            total_read += bytes_read;
+            count -= bytes_read;
+        }
+
+        return total_read;
+    }
+} // namespace Hamster
+
---
+++ src/syscall/sys_u_z.cpp
@@ -0,0 +1,102 @@
+// Hamster U-Z system calls
+
+#include <syscall/syscall.hpp>
+#include <process/task.hpp>
+#include <process/task_vfs_fd.hpp>
+#include <abi/values.hpp>
+#include <abi/structs.hpp>
+
+namespace Hamster
+{
+    int32_t sys_write(Task &task, int32_t fd, uint32_t buf_loc, uint32_t count)
+    {
+        BaseTaskFD *user_fd = task.get_fd_table()->get_fd(fd);
+        if (!user_fd)
+            return cvt_error();
+
+        int fd_flags = user_fd->get_flags();
+
+        // Copy as many times as needed
+
+        size_t total_written = 0;
+
+        while (count > 0)
+        {
+            // Read at most to the end of the page
+            uint32_t location = buf_loc + total_written;
+            size_t to_write = std::min(count, HAMSTER_PAGE_SIZE - ((location) % HAMSTER_PAGE_SIZE));
+            uint8_t *mem = task.get_memory().make_iterator_1(location);
+            if (!mem)
+            {
+                if (total_written > 0)
+                    return total_written;
+                return cvt_error();
+            }
+
+            ssize_t bytes_written = user_fd->write(mem, to_write);
+
+            if (bytes_written < 0)
+            {
+                if (total_written > 0)
+                {
+                    // Return the total bytes read so far
+                    return total_written;
+                }
+
+                if (error == H_EAGAIN)
+                {
+                    // Blocking read, block only if O_NONBLOCK isn't set and we aren't already
+                    // blocking
+                    if ((fd_flags & OPEN_NONBLOCK) == 0 && !task.is_blocking())
+                    {
+                        // Save FD for later, since it's overwritten when we return
+                        task.get_blocking_saved()[0] = fd;
+                        task.block([](Task &task) {
+                            int32_t blocking_fd = task.get_blocking_saved()[0];
+
+                            BaseTaskFD *task_fd = task.get_fd_table()->get_fd(blocking_fd);
+                            int res;
+
+                            if (!task_fd)
+                                res = -1;
+                            else
+                                res = task_fd->poll(0x2); // WRITE
+
+                            if (res == 0)
+                                return; // not ready
+                            else if (res == 1)
+                            {
+                                task.get_emulator().x[10];
+                                task.get_emulator().x[10] = syscall(task, sys_write);
+                                if ((int32_t)task.get_emulator().x[10] == -H_EAGAIN)
+                                    return;
+                            }
+                            else
+                            {
+                                // error
+                                task.get_emulator().x[10] = res;
+                            }
+
+
+                            task.end_block();
+                        });
+
+                        return 0;
+                    }
+
+                    return -H_EAGAIN;
+                }
+                return cvt_error(); // Return error if it isn't EAGAIN
+            }
+
+            if (bytes_written == 0)
+                break;
+
+            total_written += bytes_written;
+            count -= bytes_written;
+        }
+
+        return total_written;
+    }
+} // namespace Hamster
+
